a8b6b19d162fa1b3ed1b17fd63425533642db475
XulbuX-dev <142539516+XulbuX-dev@users.noreply.github.com>
2025-01-07 14:27:18 +0100

    x

--- DatapackUpdater/update-datapack.py
+++ DatapackUpdater/update-datapack.py
@@ -1,18 +1,29 @@
 from xulbux import Console, String, Regex
 from pathlib import Path
+import regex as rx
 import sys
-import re
 
 
 REGEX = {
-    "hexa": re.compile(r"(#|0x)([0-9a-fA-F]{8}|[0-9a-fA-F]{6}|[0-9a-fA-F]{3,4})\b"),
-    "enchantment_glint": re.compile(r"Enchantments\s*:\s*\[\s*\{\s*\}\s*\]"),
-    "unbreakable": re.compile(r"Unbreakable\s*:\s*1"),
-    "tags": re.compile(r"Tags\s*:\s*\[\s*(?:\"(.*)\")+\s*\]"),
-    "enchantments": re.compile(
+    "hex": rx.compile(r"(#|0x)([0-9a-fA-F]{8}|[0-9a-fA-F]{6}|[0-9a-fA-F]{3,4})\b"),
+    "nbt": rx.compile(
+        r"(?<=(?:give\s+(?:\@[apres]|[^\s]+)\s+[\w_]+\s*)|(?:nbt\s*=\s*))"
+        + Regex.brackets("{", "}", is_group=True)
+    ),
+    "enchantment_glint": rx.compile(r"Enchantments\s*:\s*\[\s*\{\s*\}\s*\]"),
+    "unbreakable": rx.compile(r"Unbreakable\s*:\s*1"),
+    "tags": rx.compile(r"Tags\s*:\s*\[\s*(?:\"(.*)\")+\s*\]"),
+    "enchantments": rx.compile(
         r"Enchantments\s*:\s*\[\s*(?:"
         + Regex.brackets("{", "}", is_group=True)
         + r"\s*,?\s*)+\s*\]"
+    ),
+    "display": rx.compile(
+        r"display\s*:\s*\{\s*Name\s*:\s*'\s*"
+        + Regex.brackets("[", "]", is_group=True)
+        + r"\s*'\s*,\s*Lore\s*:\s*"
+        + Regex.brackets("[", "]", is_group=True)
+        + r"\s*\}"
     ),
 }
 
@@ -30,8 +41,8 @@ def update_content(content: str) -> tuple[str, int]:
     changed = 0
 
     def update_enchantment(enchantment: str) -> str:
-        lvl = re.search(r"lvl\s*:\s*[0-9]+", enchantment)
-        eid = re.search(r"id\s*:\s*\"[\w_]+\"\s*,\s*", enchantment)
+        lvl = rx.search(r"lvl\s*:\s*[0-9]+", enchantment)
+        eid = rx.search(r"id\s*:\s*\"[\w_]+\"\s*,\s*", enchantment)
         return f"{eid.group()}:{lvl.group()}" if lvl and eid else enchantment
 
     def update_nbt(nbt: str) -> str:
@@ -43,41 +54,52 @@ def update_content(content: str) -> tuple[str, int]:
             + "}",
             nbt,
         )
+        nbt = REGEX["enchantments"].sub(
+            lambda m: "enchantments={levels:{"
+            + ",".join(update_enchantment(enchantment) for enchantment in m.groups())
+            + "}}",
+            nbt,
+        )
+        nbt = REGEX["display"].sub(
+            lambda m: f"custom_name='[\"\",{m.group(1)}]',lore=[{m.group(2)}]", nbt
+        )
+        return nbt
 
-    content = REGEX["hexa"].sub(lambda m: f"{m.group(1)}{m.group(2).upper()}", content)
+    content = REGEX["hex"].sub(lambda m: f"{m.group(1)}{m.group(2).upper()}", content)
+    content = update_nbt(content)
     return content, changed
 
 
-def process_file(file_path: Path, root_dir: Path = Path.cwd()) -> None:
+def process_file(file_path: Path, root_dir: Path) -> None:
     if not is_readable(file_path):
         return
-    try:
-        content = file_path.read_text(encoding="utf-8")
-        new_content, modified = update_content(content)
-        if modified:
-            file_path.write_text(new_content, encoding="utf-8")
-        log_path = str(file_path.relative_to(root_dir))
-        dim = "[dim]" if modified < 1 else ""
-        Console.done(
-            f"{'[b](Updated)' if modified > 0 else '[dim](Checked)'} [br:cyan]({log_path})"
-            + f" [dim]({((Console.w() - 50) - len(log_path)) * '.'})"
-            + f" {dim}[blue][[b|br:blue]({modified}){dim}[blue]][_]",
-            start="",
-            end="\n",
-        )
-    except Exception as e:
-        Console.fail(
-            f"Error processing [red]({file_path})\n         \t[b|br:red]{e}[_]",
-            start="",
-            end="\n",
-            exit=False,
-        )
+    # try:
+    content = file_path.read_text(encoding="utf-8")
+    new_content, modified = update_content(content)
+    if modified:
+        file_path.write_text(new_content, encoding="utf-8")
+    log_path = str(file_path.relative_to(root_dir))
+    dim = "[dim]" if modified < 1 else ""
+    Console.done(
+        f"{'[b](Updated)' if modified > 0 else '[dim](Checked)'} [br:cyan]({log_path})"
+        + f" [dim]({((Console.w() - 50) - len(log_path)) * '.'})"
+        + f" {dim}[blue][[b|br:blue]({modified}){dim}[blue]][_]",
+        start="",
+        end="\n",
+    )
+    # except Exception as e:
+    #     Console.fail(
+    #         f"Error processing [red]({file_path})\n         \t[b|br:red]{e}[_]",
+    #         start="",
+    #         end="\n",
+    #         exit=False,
+    #     )
 
 
 def main(path: str) -> None:
     target = Path(path)
     if target.is_file():
-        process_file(target)
+        process_file(target, target.parent)
     elif target.is_dir():
         for file_path in target.rglob("*"):
             if file_path.is_file():
