ee08487336b4eaa4488d47deb600abbcdacefa2c
XulbuX-dev <142539516+XulbuX-dev@users.noreply.github.com>
2025-01-10 13:24:13 +0100

    x

--- DatapackUpdater/update-datapack.py
+++ DatapackUpdater/update-datapack.py
@@ -229,6 +229,7 @@ class NamingConvention:
         content = REGEX["team_usage"].sub(
             lambda m: String.to_delimited_case(m.group(1)), content
         )
+        print(REGEX["score_usage"].findall(content))
         content = REGEX["score_usage"].sub(
             lambda m: "{"
             + ",".join(
