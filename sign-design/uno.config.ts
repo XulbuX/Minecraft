bfe7eaf031c5b73f6f7fdda3a0abc6fdc119ddaa
XulbuX <142539516+XulbuX-dev@users.noreply.github.com>
2025-04-02 20:20:02 +0200

    x

--- sign-design/uno.config.ts
+++ sign-design/uno.config.ts
@@ -1,9 +1,7 @@
 import { defineConfig, presetTagify, presetWind3 } from 'unocss';
-import { presetScrollbar } from 'unocss-preset-scrollbar';
 
 export default defineConfig({
   presets: [
-    presetScrollbar(),
     presetWind3(),
     presetTagify(),
   ],
